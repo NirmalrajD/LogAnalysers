8eb29f4d930b02eecf11230cee3789b9d6cec9ff
Battula Anjaneyulu <battula.anjaneyulu@prodapt.com>
2019-02-08 17:14:39 +0530

    PostLoggerServiceLayer has been added

---
+++ PostLoggerServiceLayer/DBAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostLoggerServiceLayer
+{
+    class DBAccess
+    {
+        public DBAccess() { }
+
+        public void PostData()
+        {
+            
+        }
+
+    }
+
+
+    public class Logger
+    {
+        public long ID { get; set; }
+        public string Level { get; set; }
+        public DateTime Datetime { get; set; }
+        public string Type { get; set; }
+        public long EventId { get; set; }
+    }
+
+    public class EventLogging
+    {
+        public long EventId { get; set; }
+        public string Location { get; set; }
+        public string Server { get; set; }
+
+    }
+
+}
+
